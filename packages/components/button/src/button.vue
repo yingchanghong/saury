6547fa51d92e48314962c92fade61c9197e63d3d
yingchanghong <1792827383@qq.com>
2023-02-18 10:46:12 +0800

    ds

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -5,15 +5,20 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { buttonProps } from './props';
+
 export default defineComponent({
   name:'SyButton',
   props:buttonProps,
   setup(props){
+    const a = 12;
     console.log(1)
   }
 })
 </script>
 
 <style>
-
+.a {
+  color: red;
+  width: 10px;
+}
 </style>
