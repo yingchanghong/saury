54a8b89884d50d6901b2ea4b7426ca1db832db2e
花海里的秋刀鱼 <1157570186@qq.com>
2023-02-20 20:35:11 +0800

    feat：class命名、props

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -1,5 +1,7 @@
 <template>
-  <button>按钮sss</button>
+  <button :class="[n(), n(type)]">
+    <span><slot /></span>
+  </button>
 </template>
 
 <script lang="ts">
@@ -9,15 +11,23 @@ export default defineComponent({
   name: 'SyButton',
   props: buttonProps,
   setup(props) {
+    const { type } = props
+    const nameSpace = (compent: string) => {
+      let cmName = compent
+      return (name?: string) => {
+        let className = `sy-${cmName}`
+        if (name) className += `-${name}`
+        return className
+      }
+    }
+    const n = nameSpace('btn')
     const a = 1
-    console.log(a)
+    console.log(props.type)
+    return { type, n }
   },
 })
 </script>
 
 <style lang="less">
-.a {
-  width: 10px;
-  color: red;
-}
+@import './index.less';
 </style>
--- packages/components/button/src/props.ts
+++ packages/components/button/src/props.ts
@@ -1,8 +1,28 @@
 import type { ExtractPropTypes } from 'vue'
 
 export const buttonProps = {
-  text: {
+  disabled: {
+    type: Boolean,
+  },
+  icon: {
     type: String,
+  },
+  onClick: {
+    type: Function,
+  },
+  type: {
+    type: String, // primary | ghost | link | dashed
+    default: 'default',
+  },
+  loading: {
+    type: Boolean,
+  },
+  color: {
+    type: String,
+  },
+  size: {
+    type: String, // 'laege'|'default'|'small'
+    default: 'default',
   },
 } as const
 
