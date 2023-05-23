f2fa1c05cfc2408db6ae9311a16fab96d3098aa8
yingchanghong <yingchanghong>
2023-05-23 18:34:16 +0800

    feat: volar代码提示

---
+++ volar.d.ts
@@ -0,0 +1,10 @@
+// Auto generated component declarations
+declare module 'vue' {
+  export interface GlobalComponents {
+    SyButton: typeof import('saury-ui')['SyButton']
+    SyIcon: typeof import('saury-ui')['SyIcon']
+    SyInput: typeof import('saury-ui')['SyInput']
+    SyTab: typeof import('saury-ui')['SyTab']
+  }
+}
+export {}
